e4fc8eb020f4c8e34927e73839339420c4c42ae4
unknown <bartosz.termena@gmail.com>
2019-06-28 07:37:44 +0200

    Adding posts to post-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mean-stack';
+    storedPosts = [];
+
+    onPostAdded(post) {
+        this.storedPosts.push(post);
+    }
 }
